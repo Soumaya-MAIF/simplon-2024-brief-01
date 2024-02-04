c904f983a293f8f4ee3e866c5d88c156c505ddc0
Soum <dev.web21@outlook.fr>
2024-02-04 17:41:25 +0100

    correction code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import '../design/Header.css';
 
 export default function Header() {
+
+  
+
   return (
     <header>
       <nav>
         <div className="logo">LogoHébergement</div>
-        <ul className="nav-links">
-          <li><a href="/">Accueil</a></li>
-          <li><a href="/#features">Fonctionnalités</a></li>
-          <li><a href="/#contact">Contact</a></li>
-        </ul>
+        <div id="mySidenav" class="sidenav">
+            <a id="closeBtn" href="#" class="close"></a>
+            <div id = "menu" > 
+                <ul className="nav-links">
+                  <li><a href="/">Accueil</a></li>
+                  <li><a href="/#features">Fonctionnalités</a></li>
+                  <li><a href="/#contact">Contact</a></li>
+                </ul>
+            </div >
+          </div>
       </nav>
       <section className="hero">
+         
         <h1>Bienvenue sur Plateforme d'Hébergement Web</h1>
         <p>Le meilleur choix pour héberger vos sites web en toute sécurité.</p>
         <a href="#pricing" className="cta">Voir les prix</a>
@@ -20,3 +29,4 @@ export default function Header() {
     </header>
   );
 }
+              
